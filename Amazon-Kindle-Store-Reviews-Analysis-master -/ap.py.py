7b68d7a92c1d69acc193b9c6adb545ebbc228d06
Margipatel43 <78413366+Margipatel43@users.noreply.github.com>
2021-04-17 19:49:16 +0530

    Add files via upload

---
+++ Amazon-Kindle-Store-Reviews-Analysis-master -/ap.py.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 12:17:32 2020
+@author: hp 620 tx
+"""
+import numpy as np
+import os
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+global graph #global graph
+graph = tf.get_default_graph()
+from flask import Flask , request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+
+from flask import Flask,request,render_template # request is post in html page
+from keras.models import load_model
+import numpy as np
+global model,graph #global variables
+import tensorflow as tf
+graph = tf.get_default_graph()
+model = load_model('nlpmodel.h5')
+app = Flask(__name__)
+@app.route('/')#whenever browser finds localhost:5000 then execute below function 
+def home(): 
+    return render_template('index1.html') # this function is returning index.html file
+@app.route('/login',methods=['POST']) # when you click submit on html page it is redirecting to this url.
+def login():  # as soon as this url is directed then call this function
+    A=request.form['a'] # from html page whatever the text is typed that is requested from the 'form' functionality and is stored in a main variable 
+    B=request.form['b']
+    total = [[a,b]]
+    with graph.as_default():
+        ypred=model.predict(np.array(total))
+        print(ypred)
+        y=ypred[0][0]
+    E=str(A)+str(B)+str(C)+str(D)
+    return render_template('index1.html',abc = y) # after typing the name, show this name on index.html file where we have created variable abc
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ Amazon-Kindle-Store-Reviews-Analysis-master -/program.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 11:34:12 2020
+
+@author: hp 620 tx
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset=pd.read_csv('data.tsv',delimiter='\t',quoting=3)
+#quoting = 0 for quoting minimal values
+#quoting = 1 for quoting all the values
+#quoting = 2 for non-numerical values
+#quoting = 3 for quoting none(considering no quotations in dataset)
+# re- Regular Expression library (to remove unwanted dots and extra characters which wont be used for prediction)
+# sub method to remove dots and consider only alphabets
+# nltk library - Natural Language Tool Kit  
+#download stopwords method from nltk (for present dataset only english stopwords)
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords  #corpus - grouping the texts
+from nltk.stem.porter import PorterStemmer
+dataset['reviewText']=dataset['reviewText'].apply(str)
+#tokenizer = nltk.RegexpTokenizer(r"\w+")
+#new_words = tokenizer.tokenize(dataset['reviewText'])
+
+cps=[] #empty list to append all the converted words
+for i in range(0,982619):  #since there are 1000 reviews
+    review=re.sub('[^a-zA-Z]',' ',dataset['reviewText'][i])
+#Line 19 - importing regular expresion library
+#line 26 - Taking Review variable and seperating values with giving spaces by considering only alphabets in Zeroth Row using sub method(to remove dots)
+#line 30 - to convert into small letters
+    review=review.lower()
+    review=review.split() # convert string into list
+#to remove stop words in one single sentence and to get the words which are not there in stopwords
+    review=[word for word in review if not word in set(stopwords.words('english'))]
+#stemming concepts - removes ed,ly...extensions of words 
+    ps=PorterStemmer() #both stemming and lemmatisation takesplace at a time
+    review=[ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review=' '.join(review) #to join all words which are in review with ' 'space in list
+    cps.append(review)
+from sklearn.feature_extraction.text import CountVectorizer #for creating bag of words - for counting different vectors which are there in particular list and making them as different columns and we will be counting all the values if the repetition values are there and noting them under particular column 
+cv=CountVectorizer(max_features=982550) #we are having 1565 words in total i.e omitting 65 elements or attributes which are created and making it more precise about it
+#x,y two arrays as input and output and then giving to ann model
+#o's and 1's are just number of repetitions- - t can be any number that word is repeated
+# Most of them are zeros here because they are not repeated in those sentences - its called as sparsity or sparse matrix - to remove such kind of dependencies parameter called max_features is used.
+x=cv.fit_transform(cps).toarray()
+y=dataset.iloc[:,-2].values
+# Splitting data into training and testing parts
+from sklearn.model_selection import train_test_split
+x.shape
+y.shape
+y.reshape(-1,1)
+y.shape
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=0)
+#now using ANN model to predict
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+model=Sequential()
+model.add(Dense(input_dim=1500,init="random_uniform",activation="sigmoid",output_dim=1000))#input layer
+model.add(Dense(input_dim=100,init="random_uniform",activation="sigmoid",output_dim=100))#hidden layer
+model.add(Dense(input_dim=1,init="random_uniform",activation="sigmoid",output_dim=1))#output layer
+model.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])#adam-batch descent
+model.fit(x_train,y_train,epochs=50000,batch_size=10)#epochs-number of iterations
+y_pred=model.predict(x_test)
+y_pred=(y_pred>0.5)
+
+from sklearn.metrics import confusion_matrix
+cn=confusion_matrix(y_test,y_pred)
+
+from sklearn.metrics import accuracy_score
+accuracy=accuracy_score(y_test,y_pred)
